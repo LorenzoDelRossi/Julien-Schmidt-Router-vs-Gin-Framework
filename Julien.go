87176fc33e3345bebb6ec89f5d4c02b7d6daead9
LorenzoDelRossi <50426571+LorenzoDelRossi@users.noreply.github.com>
2019-10-02 10:12:53 +0200

    Update Julien.go

--- Julien.go
+++ Julien.go
@@ -4,15 +4,12 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"log"
-	"time"
 	"net/url"
 )
 
 func main() {
 	router := httprouter.New()
 	router.POST("/v1/add", posthandler)
-	go juliengo(500)
 	http.ListenAndServe(":9001", router)
 
 }
@@ -20,20 +17,4 @@ func main() {
 func posthandler(w http.ResponseWriter, r *http.Request, c httprouter.Params) {
 	r.ParseForm()
 	fmt.Println("Response: ", r.PostForm.Get("name"))
-}
-
-//juliengo sends N POST requests to the Julien server on port :9001 and takes the time spent in the handling process
-func juliengo(N int) {
-	var i int
-	starts := time.Now()
-	for i = 0; i < N; i++ {
-		go func() {
-			_, err := http.PostForm("http://localhost:9001/v1/add", url.Values{"name": {"prova"}})
-			if err != nil {
-				log.Fatalln(err)
-			}
-		}()
-	}
-	fmt.Println("CHIAMATE CONCORRENTI GESTITE:", i)
-	fmt.Println("TEMPO IN JULIENGO: ", time.Since(starts))
 }
