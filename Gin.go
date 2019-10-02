c4bc8f5ce69a65c8f5100c20e1e3314821a5a13f
LorenzoDelRossi <50426571+LorenzoDelRossi@users.noreply.github.com>
2019-10-02 10:11:39 +0200

    Update Gin.go

--- Gin.go
+++ Gin.go
@@ -16,23 +16,6 @@ func main() {
 		message := c.Request.PostFormValue("name")
 		fmt.Println("Response: ", message)
 	})
-	go gingo(500)
 	r.Run(":9002") // listen on port :9002
 }
 
-//gingo sends N POST requests to the Gin server on port :9002 and takes the time spent in the handling process
-func gingo(N int) {
-	var i int
-	starts := time.Now()
-	for i = 0; i < N; i++ {
-		go func() {
-			_, err := http.PostForm("http://localhost:9002/v1/add", url.Values{"name": {"prova"}})
-			if err != nil {
-				//handle postform error
-				log.Fatalln(err)
-			}
-		}()
-	}
-	fmt.Println("CHIAMATE CONCORRENTI GESTITE: ", i)
-	fmt.Println("TEMPO IN GINGO: ", time.Since(starts))
-}
